5defe5dff1626b909ed6fba2570dd1e5d0dd0eae
smallhorse87 <smallhorse87@163.com>
2018-12-29 20:53:50 +0800

    Initial Commit

---
+++ nortchExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nortchExample
+//
+//  Created by mac on 2018/12/29.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nortchExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nortchExample
+//
+//  Created by mac on 2018/12/29.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
